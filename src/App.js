d1bae72798e3f0ab36afaa95188fe98e78cb5a1b
MelanieLezcano <melanie.s.lezcano@gmail.com>
2023-05-21 18:20:15 -0300

    importo PokemonGallery.jsx en ap.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-import
+import React from "react";
+import PokemonGallery from "./components/PokemonGallery";
+
+
+function app() {
+    return(
+        <div>
+            <h1>Desafío Pokedéx</h1>
+            <PokemonGallery/>
+        </div>
+    );
+}
 
 export default App;

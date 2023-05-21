49f80f6f0847ad537f474b440d2dbac0d6684981
MelanieLezcano <melanie.s.lezcano@gmail.com>
2023-05-21 19:21:22 -0300

    instalo fetch, lista con api pokemon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import PokemonGallery from "./components/PokemonGallery";
 
 
-function app() {
+function App() {
     return(
         <div>
             <h1>Desafío Pokedéx</h1>
--- src/components/PokemonGallery.jsx
+++ src/components/PokemonGallery.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PokemonGallery() {
-    return(
+    const [pokemonList, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        console.log('%cSe montó el componente', 'color: pink');
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+            .then(response => response.json())
+            .then(data => {
+                setPokemonList(data.results);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    useEffect(() => {
+        console.log('%cSe actualizó el componente', 'color: pink');
+    }, [pokemonList])
+
+    return (
         <div>
-            {}
+            {pokemonList.length === 0 && <p>Cargando</p>}
+            {
+                pokemonList.map((pokemon, i) => {
+                    return (
+                        <div key={i}>
+                            <div>{pokemon.name}</div>
+                        </div>
+                    )
+                })
+            }
+
         </div>
     );
 }

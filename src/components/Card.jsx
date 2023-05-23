e6bb3c8a8c51cde722b28ebe00ab00be7ab2f507
MelanieLezcano <melanie.s.lezcano@gmail.com>
2023-05-22 23:42:24 -0300

    css cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styling/Card.css";
+
+function Card({ pokemon }) {
+  return (
+    <div className="card">
+      <div className="cardImagePosition">
+        <div className="imagePokemon">
+        <img src={pokemon.imageUrl} alt={pokemon.name} />
+        </div>
+      </div>
+      <div className="name">{pokemon.name}</div>
+      <div className="weight">Peso: {pokemon.weight}</div>
+      <div className="pokemonAbilities">
+        <h4>Habilidades:</h4>
+        <ul>
+          {pokemon.abilities.map((ability, index) => (
+            <li key={index}>-{ability}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
